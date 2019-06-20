fd9d51d0a7e58c5296d94c931d000842135418ac
Ferenc Szontágh <szf@ghostlinux.hu>
2019-06-20 15:31:26 +0200

    addedd attr

---
+++ attr.sh
@@ -0,0 +1,37 @@
+#
+# Package: attr
+# 
+#
+file_name="${name}"-"${version}".tar.gz
+url=http://quantum-mirror.hu/mirrors/pub/gnusavannah/attr/${file_name}
+strip=1
+arch=x86_64
+configure_options="--prefix=/usr     \
+            --bindir=/bin     \
+            --disable-static  \
+            --sysconfdir=/etc \
+            --docdir=/usr/share/doc/$name-$version"
+
+
+function post_make() {
+	cd ${1}
+	[ -e "${1}/lib" ] || mkdir ${1}/lib
+	
+	mv -v ${1}/usr/lib/libattr.so.* ${1}/lib
+	ln -sfv ${1}../../lib/$(readlink /usr/lib/libattr.so) ${1}/usr/lib/libattr.so
+}
+
+function pre_make() {
+		echo "Executing make..."
+}
+
+function configure() {
+		echo "Configuring in : ${1}..."
+		cd ${1}
+		./configure ${configure_options} 
+}
+
+function build() {
+		echo "Starting build... ${name} in ${SOURCES}"
+		make -j${NUMCPU} && make DESTDIR="${1}" install
+}
