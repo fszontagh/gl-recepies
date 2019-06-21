cc25d572de685306d4b39e8793db9506e80495f4
Ferenc Szontágh <szf@ghostlinux.hu>
2019-06-21 07:48:21 +0200

    added bc

---
+++ bc.sh
@@ -0,0 +1,58 @@
+#
+# Package: bc
+# Tested version: bc 1.07.1
+# 
+file_name="${name}"-"${version}".tar.gz
+url=http://ftp.gnu.org/gnu/bc/${file_name}
+strip=1
+arch=x86_64
+#the default configure options
+configure_options=" \
+			--prefix=/usr           \
+            --with-readline         \
+            --mandir=/usr/share/man \
+            --infodir=/usr/share/info"
+
+
+function post_make() {
+	#${PKG} is the package dir
+	#${SOURCE_DIR} is the source dir, where run the compile and the make
+	#${1} is equal with the ${SOURCE_DIR}
+}
+
+function pre_make() {
+	#${PKG} is the package dir
+	#${SOURCE_DIR} is the source dir, where run the compile and the make
+	#${1} there is no parameter to the function.
+	# This function called before configure on the source
+	cd ${SOURCE_DIR}
+cat > bc/fix-libmath_h << "EOF"
+#! /bin/bash
+sed -e '1   s/^/{"/' \
+    -e     's/$/",/' \
+    -e '2,$ s/^/"/'  \
+    -e   '$ d'       \
+    -i libmath.h
+
+sed -e '$ s/$/0}/' \
+    -i libmath.h
+EOF
+
+sed -i -e '/flex/s/as_fn_error/: ;; # &/' configure	
+}
+
+function configure() {
+	#${1} is the source dir
+	#${SOURCE_DIR} is equal with the ${1}
+	echo "Configuring in : ${1}..."
+	cd ${1}
+	./configure ${configure_options}
+}
+
+function build() {
+	#${1} is the target directory, where the raw binaries will be saved
+	#${SOURCE_DIR} is the sources dir
+	# Default, the make running in the ${SOURCE_DIR}, because the configure function cding into this dir
+	echo "Starting build... ${name} in ${SOURCES}"
+	make -j${NUMCPU} && make DESTDIR="${1}" install
+}
