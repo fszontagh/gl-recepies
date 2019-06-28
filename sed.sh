0c6304488a156a26632bd793c260ff230a540829
szf@ghostlinux.hu <szf@ghostlinux.hu>
2019-06-28 10:57:35 +0200

    added sed

---
+++ sed.sh
@@ -0,0 +1,46 @@
+#
+# Package: sed
+# Tested version: sed 4.7
+#
+file_name="${name}"-"${version}".tar.xz
+url=http://ftp.gnu.org/gnu/sed/${file_name}
+strip=1
+arch=x86_64
+#the default configure options
+configure_options="--prefix=/usr --bindir=/bin --libdir=/lib --htmldir=/usr/share/doc/sed-${version}"
+
+
+function post_make() {
+	#${PKG} is the package dir
+	#${SOURCE_DIR} is the source dir, where run the compile and the make
+	#${1} is equal with the ${SOURCE_DIR}	
+	rm -rf ${PKG}/usr/share/info/dir
+}
+
+function pre_make() {
+	#${PKG} is the package dir
+	#${SOURCE_DIR} is the source dir, where run the compile and the make
+	#${1} there is no parameter to the function.
+	# This function called before configure on the source
+	echo -en ""
+}
+
+function configure() {
+	#${1} is the source dir
+	#${SOURCE_DIR} is equal with the ${1}
+	echo "Configuring in : ${1}..."
+	cd ${1}
+	./configure ${configure_options}
+}
+
+function build() {
+	#${1} is the target directory, where the raw binaries will be saved
+	#${SOURCE_DIR} is the sources dir
+	# Default, the make running in the ${SOURCE_DIR}, because the configure function cding into this dir
+	echo "Starting build... ${name} in ${SOURCES}"
+	make -j${NUMCPU}
+	make html
+	make check
+	make DESTDIR="${1}" install
+	make -C doc DESTDIR=${PKG} install-html
+}
