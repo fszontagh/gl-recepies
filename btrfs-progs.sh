024af2f5508b2a26975c77928849bc629f3ab16b
szf@ghostlinux.hu <szf@ghostlinux.hu>
2019-06-28 15:27:07 +0200

    added btrfs-progs

---
+++ btrfs-progs.sh
@@ -0,0 +1,42 @@
+#
+# Package: btrfs-progs
+# Tested version: btrfs-progs v4.20.2
+#
+file_name="${name}"-"${version}".tar.xz
+url=https://mirrors.edge.kernel.org/pub/linux/kernel/people/kdave/btrfs-progs/${file_name}
+strip=1
+arch=x86_64
+#the default configure options
+configure_options="--prefix=/usr libdir=/lib bindir=/sbin"
+
+
+function post_make() {
+	#${PKG} is the package dir
+	#${SOURCE_DIR} is the source dir, where run the compile and the make
+	#${1} is equal with the ${SOURCE_DIR}
+	echo -en ""	
+}
+
+function pre_make() {
+	#${PKG} is the package dir
+	#${SOURCE_DIR} is the source dir, where run the compile and the make
+	#${1} there is no parameter to the function.
+	# This function called before configure on the source
+	echo -en ""
+}
+
+function configure() {
+	#${1} is the source dir
+	#${SOURCE_DIR} is equal with the ${1}
+	echo "Configuring in : ${1}..."
+	cd ${1}
+	./configure ${configure_options}
+}
+
+function build() {
+	#${1} is the target directory, where the raw binaries will be saved
+	#${SOURCE_DIR} is the sources dir
+	# Default, the make running in the ${SOURCE_DIR}, because the configure function cding into this dir
+	echo "Starting build... ${name} in ${SOURCES}"
+	make -j${NUMCPU} && make DESTDIR="${1}" install
+}
